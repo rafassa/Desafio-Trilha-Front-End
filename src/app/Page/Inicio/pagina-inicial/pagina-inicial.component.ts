5af8d0df005d679dba7621c986a4f3ab22b386de
rafassa <rafassa95@gmail.com>
2025-05-20 20:24:08 -0300

    tentativa de mudar pagina inicial

--- src/app/Page/Inicio/pagina-inicial/pagina-inicial.component.ts
+++ src/app/Page/Inicio/pagina-inicial/pagina-inicial.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [CarouselComponent, CommonModule, FormsModule,ReactiveFormsModule],
+  imports: [CarouselComponent, CommonModule, FormsModule,ReactiveFormsModule, RouterModule],
   templateUrl: './pagina-inicial.component.html',
   styleUrl: './pagina-inicial.component.css'
 })
