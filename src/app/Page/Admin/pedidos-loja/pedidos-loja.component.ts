957efb1d52ae8f50db6eacb4dc02ea6ca57d7412
rafassa <rafassa95@gmail.com>
2025-05-30 09:01:20 -0300

    alterando logica do service e do ts dos pedidos da loja

--- src/app/Page/Admin/pedidos-loja/pedidos-loja.component.ts
+++ src/app/Page/Admin/pedidos-loja/pedidos-loja.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Produto } from '../../../Interface/Produto.interface';
 import { CommonModule } from '@angular/common';
 import { BarraLateralComponent } from '../barra-lateral/barra-lateral.component';
+import { PedidosLojaService } from '../../../services/pedidos-loja.service';
 
 @Component({
   selector: 'app-pedidos-loja',
@@ -12,6 +13,36 @@ import { BarraLateralComponent } from '../barra-lateral/barra-lateral.component'
 export class PedidosLojaComponent {
 
 
+ service = inject(PedidosLojaService)
+
+produtos:Produto[]=[]
+nome:string =''
+valor:number=0
+
+pedidos: {produtos:Produto[], nome:string, valor:number}[]=[]
+
+ngOnInit(){
+  this.pegarPedido()
+
+  this.pedidos = this.service.devolverPedido()
+}
+
+pegarPedido(){
+  const produtoPedido = localStorage.getItem('itemPedido')
+  const nomePedido = localStorage.getItem('nomePedido')
+  const valorPedido= localStorage.getItem('valorPedido')
+
+
+  if(produtoPedido && nomePedido && valorPedido){
+    this.produtos = JSON.parse(produtoPedido)
+    this.nome = JSON.parse(nomePedido)
+    this.valor = JSON.parse(valorPedido)
+  }
+
+this.service.pegarInformacao(this.produtos, this.nome, this.valor)
+  
+
+}
 
 
 }
--- src/app/services/pedidos-loja.service.ts
+++ src/app/services/pedidos-loja.service.ts
@@ -6,4 +6,24 @@ import { Produto } from '../Interface/Produto.interface';
 })
 export class PedidosLojaService {
 
+
+
+  private pedidos: {produtos:Produto[], nome:string, valor:number}[]=[]
+
+
+  pegarInformacao(produtos:Produto[], nome:string, valor:number){
+      const pedidoPassado = {
+        produtos:produtos,
+        nome:nome,
+        valor:valor
+      }
+
+      this.pedidos.push(pedidoPassado)
+
+  }
+
+
+  devolverPedido(){
+    return this.pedidos
+  }
 }
